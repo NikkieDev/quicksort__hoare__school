5f119cccaa095ca88c8846bba11cc8bffd5bc882
Nikkie Schaad <nikkie@schaad.zip>
2025-01-28 13:19:10 +0100

    sort works, now make recurse

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #define CURSOR_BIGGER_THAN_SIZE -1;
+#define SUCCESS 0;
+#define FALSE 0;
+#define TRUE 1;
 
 double avg(int *arr, size_t arr_size)
 {
@@ -10,18 +17,17 @@ double avg(int *arr, size_t arr_size)
     return total / arr_size;
 }
 
-signed int append(int *arr, size_t *cur, size_t size, int num)
+signed int append(int *arr, size_t *cur, size_t size, int num) // -> make struct for arrays
 {
-    if ((*cur) > size) return CURSOR_BIGGER_THAN_SIZE;
+    if ((*cur) >= size) return CURSOR_BIGGER_THAN_SIZE;
     arr[*cur] = num;
     (*cur)++;
 
-    return 0;
+    return SUCCESS;
 }
 
 void *part(int *arr, size_t arr_size)
 {
-    int arr_filling = 0;
     double average = avg(arr, arr_size);
 
     size_t higher_size = 0;
@@ -29,22 +35,24 @@ void *part(int *arr, size_t arr_size)
     size_t loweq_size = 0;
     size_t loweq_cursor = 0;
 
-    for (int i = 0; i < arr_size; i++) {
-        if (arr[i] > averge) {
+    for (int i =0; i < arr_size; i++) {
+        if (arr[i] > average) {
             higher_size++;
-            if (arr_filling) {
-                append(higher, &higher_cursor, higher_size, arr[i]);
-            }
         } else {
-            if (arr_filling) {
-                append(loweq, &loweq_cursor, loweq_size, arr[i]);
-            }
             loweq_size++;
         }
+    }
 
-        if (i == arr_size && !arr_filling) {
-            i = 0;
-            arr_filling = 1;
+    int *higher = malloc(higher_size * sizeof(int));
+    int *loweq = malloc(loweq_size * sizeof(int));
+
+    for (int i = 0; i < arr_size; i++) {
+        if (arr[i] > average) {
+            printf("Appending %d to higher than %f\n", arr[i], average);
+            append(higher, &higher_cursor, higher_size, arr[i]);
+        } else {
+            printf("Appending %d to loweq than %f\n", arr[i], average);
+            append(loweq, &loweq_cursor, loweq_size, arr[i]);
         }
     }
 }
@@ -54,13 +62,11 @@ int main(void)
     size_t numbers_size = 6;
     int numbers[6] = { 3, 1, 12, 95, 120, 4 };
 
-    int numbers_sorted[arr_size] = malloc(sizeof(int) * arr_size);
+    part(numbers, numbers_size);
 
-    
+    // int numbers_sorted[numbers_size];
 
-    for (int i = 0; i < arr_size; i++) {
-        printf("%i. %d\n", i+1, numbers_sorted[i]);
-    }
-
-    free(numbers_sorted);
+    // for (int i = 0; i < numbers_size; i++) {
+    //     printf("%i. %d\n", i+1, numbers_sorted[i]);
+    // }
 }
